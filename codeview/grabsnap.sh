89a7c22afa321ff36caab510d946f19f7e7c8a92
Alexander Mendez <bluespace@gmail.com>
2011-05-18 10:26:08 -0700

    updated grabsnap, and fixed up the multiple monitor support

--- codeview/grabsnap.sh
+++ codeview/grabsnap.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 ## GrabSnap -- An automated screenshot and osx iSight imaging tool
 ##   Requires isightcapture in the path
+##   grabsnap.sh [name] [interval in seconds] [number of monitors]
 ## [2011.04.11] 
 
 
-
+INTERVAL=60
 PICTURE_DIR="$HOME/Pictures/grabsnap"
+MONITORS=1
 
-
-if [ ! -d $myimg ]; then
- mkdir "$myimg"
+if [ ! -z "$1" ]; then 
+	PICTURE_DIR="$PICTURE_DIR/$1"
 fi
 
 
+
+if [ ! -d "$PICTURE_DIR" ]; then
+	echo "making $PICTURE_DIR"
+ 	mkdir -p "$PICTURE_DIR"
+fi
+
+
+
+if [ ! -z "$2" ]; then
+    INTERVAL=$2
+fi
+
+if [ ! -z "$3" ]; then
+	MONITORS=$3
+fi
+
+
+echo -e "Capture Script Running every $INTERVAL seconds: "
 COUNTER=0
-if [ -z "$1" ]; then
-    INTERVAL=60
-else
-    INTERVAL=$1
-fi
-
-
-echo -e "Capture Script Running every $INTERVAL seconds: " 
 while [ $COUNTER -lt 100000 ]; do # could replace with true, but zombie protection is good.
 	PICTURE_DATE=$(date +%Y-%m-%d_%H.%M.%S)
 	isightcapture -t png "$PICTURE_DIR/SNAP_$PICTURE_DATE"
-	screencapture -T 0 -x "$PICTURE_DIR/DESK_$PICTURE_DATE.png"
-	sips --resampleWidth 960 "$PICTURE_DIR/DESK_$PICTURE_DATE.png" >>/dev/null 2>&1
+	for (( c=0; c<=$MONITORS; c++ )); do 
+		FILES[$c]="$PICTURE_DIR/DESK${c}_$PICTURE_DATE.png"
+	done
+	screencapture -C -T 0 -x ${FILES[@]}
+	for (( c=0; c<=$MONITORS; c++ )); do 
+		szips --resampleWidth 960 "$PICTURE_DIR/DESK${c}_$PICTURE_DATE.png" >>/dev/null 2>&1
+	done
+	
 	echo -e "\r $PICTURE_DATE:  Another $INTERVAL Seconds down. So far:$COUNTER"
 	
 	for (( i=$INTERVAL; i>0; i-- )); do
