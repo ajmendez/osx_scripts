63f7eef65525c8dad64f88761b5b938974036ae6
Alexander Mendez <bluespace@gmail.com>
2012-03-28 11:35:21 -0700

    could not export as a function, typedef +fx not nice

--- teatime/teatime.sh
+++ teatime/teatime.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
-## Teatime is a simple teatime script to calculate the number of minutes till the next teatime.
-teatime () {
-  TEATIME=$((15*60))
-  HR=$(($(date +%H)*60+$(date +%M)))
-  if [[ "$HR" > "$TEATIME" ]]; then 
-    echo $(($TEATIME+24*60-$HR)) Minutes till Tea Time...
-  else
-    echo $(($TEATIME-$HR)) Minutes till Tea Time
-  fi
-  exit 1;
-}
-teatime
+## Teatime is a simple teatime script to calculate the number of minutes till the next teatime.  Add it to your path, and add teatime.sh to your .bashrc/.bash_profile.
+## [Mendez 2012.03]
+TEATIME=$((15*60))
+HR=$(($(date +%H)*60+$(date +%M)))
+if [[ "$HR" > "$TEATIME" ]]; then 
+  echo $(($TEATIME+24*60-$HR)) Minutes till Tea Time...
+else
+  echo $(($TEATIME-$HR)) Minutes till Tea Time
+fi
